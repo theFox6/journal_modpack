3942a189946051f43c52b942e6531231e9b50055
theFox6 <the.fox6@gmx.de>
2018-02-08 14:54:36 +0100

    since it's player specific we don't want to give "singleplayer" the entry

--- example.lua
+++ example.lua
@@ -8,7 +8,7 @@ minetest.register_on_dignode(function(pos, oldnode, digger)
 	local name = digger:get_player_name()
 	if oldnode.name == "default:tree" then
 		if not journal.playerdata_getKey(name,"journal_foundLog") then
-			journal.add_entry("singleplayer","journal test","Today I found a log. It's nothing but a little peice of my ship I will have.")
+			journal.add_entry(name,"journal test","Today I found a log. It's nothing but a little peice of my ship I will have.")
 			journal.playerdata_setKey(name,"journal_foundLog",true)
 		end
 	end
@@ -22,7 +22,7 @@ minetest.register_on_craft(function(itemstack, player, old_craft_grid, craft_inv
 	if itemstack:get_name() == "default:wood" then
 		local name = player:get_player_name()
 		if not journal.playerdata_getKey(name,"journal_craftedPlanks") then
-			journal.add_entry("singleplayer","journal test","So I have some planks, but I will need a lot more to build my ship.")
+			journal.add_entry(name,"journal test","So I have some planks, but I will need a lot more to build my ship.")
 			journal.playerdata_setKey(name,"journal_craftedPlanks",true)
 		end
 	end
