eccc08130bbb25879d56225541ac6d4042fed1b6
theFox6 <the.fox6@gmx.de>
2018-02-09 12:18:36 +0100

    small changes

--- example.lua
+++ example.lua
@@ -3,11 +3,11 @@ journal.register_page("journal:test","ship guide","captain's log")
 journal.triggers.register_on_dig({
 	target = "default:tree",
 	is_active = function(playerName)
-		return not journal.playerdata_getKey(playerName,"journal:foundLog")
+		return not journal.playerdata_getKey(playerName,"journal:foundTree")
 	end,
 	call = function(data)
 		journal.add_entry(data.playerName,"journal:test","Today I found a log. It's nothing but a little piece for the ship that I will build.",true)
-		journal.playerdata_setKey(data.playerName,"journal:foundLog",true)
+		journal.playerdata_setKey(data.playerName,"journal:foundTree",true)
 	end,
 })
 
@@ -30,7 +30,7 @@ journal.triggers.register_on_craft({
 	call = function(data)
 		journal.add_entry(data.playerName,"journal:test","Well I have my ship now ...",true)
 		minetest.after(10,journal.add_entry,data.playerName,"journal:test","On second thoughts, I would have expected my ship to be bigger.\nMaybe out there on the open sea are some bigger ships...",true)
-		minetest.after(12,journal.add_entry,data.playerName,"journal:test","---THE END---\nI hope you liked the epic story :P",false)
+		minetest.after(15,journal.add_entry,data.playerName,"journal:test","---THE END---\nI hope you liked the epic story :P",false)
 		journal.playerdata_setKey(data.playerName,"journal:craftedBoat",true)
 	end,
 })
