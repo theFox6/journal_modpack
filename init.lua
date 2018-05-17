ff8fbf3743c145e7d1b0ded1b82bf637dc80d121
theFox6 <the.fox6@gmx.de>
2018-05-17 16:14:42 +0200

    correctly define global "journal"

--- init.lua
+++ init.lua
@@ -5,9 +5,8 @@ else
   print("[MOD] "..minetest.get_current_modname()..": loading")
 end
 
-journal = {
-	modpath = minetest.get_modpath("journal")
-}
+rawset(_G, "journal", {})
+journal.modpath = minetest.get_modpath("journal")
 
 function journal.check_modname_prefix(name)
 	if name:sub(1,1) == ":" then
