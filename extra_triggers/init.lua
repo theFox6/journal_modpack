16cfc9b8ca7b4c494cebe88a8a8c67e5c58e9de6
theFox6 <the.fox6@gmx.de>
2018-06-23 11:09:46 +0200

    on rightclick doesn't work yet

--- extra_triggers/init.lua
+++ extra_triggers/init.lua
@@ -1,43 +1,42 @@
+--[[FIXME
 journal.triggers.register_trigger("rightclick")
 local rightclickfuncs = {}
 
-local function handle_rightclick(pos, node, clicker, itemstack, pointed_thing)
-	if rightclickfuncs[node.name] then
-		rightclickfuncs[node.name](pos, node, clicker, itemstack, pointed_thing)
-	end
+function journal.triggers.handle_rightclick(pos, node, clicker, itemstack, pointed_thing)
+	local ret = rightclickfuncs[node.name](pos, node, clicker, itemstack, pointed_thing)
 
 	if not clicker or not pos or not node then
-		return
+		return ret
 	end
 
 	local name = clicker:get_player_name()
 	if not name or name == "" then
-		return
+		return ret
 	end
 
 	local data = {
 		target = node.name,
 		playerName = name,
-		tool = puncher:get_wielded_item():get_name(),
+		tool = clicker:get_wielded_item():get_name(),
 	}
 
 	journal.triggers.run_callbacks("rightclick", data)
+	return ret
 end
 
 minetest.after(3, function()
 	for name, def in pairs(minetest.registered_items) do
-		if def.on_rightclick then
+		if def.on_rightclick and def.on_rightclick~=journal.triggers.handle_rightclick then
 			rightclickfuncs[name] = def.on_rightclick
+			minetest.override_item(name,{on_rightclick=journal.triggers.handle_rightclick})
 		end
-		minetest.override_item(name,{on_rightclick=handle_rightclick})
 	end
-end)
+end)]]
 
 journal.triggers.register_trigger("pickup")
 local old_punch = minetest.registered_entities["__builtin:item"].on_punch
 minetest.registered_entities["__builtin:item"].on_punch = function(self, hitter)
 	old_punch(self, hitter)
-
 	if not hitter or not self then
 		return
 	end
@@ -54,4 +53,5 @@ minetest.registered_entities["__builtin:item"].on_punch = function(self, hitter)
 	}
 
 	journal.triggers.run_callbacks("pickup", data)
+	return
 end
